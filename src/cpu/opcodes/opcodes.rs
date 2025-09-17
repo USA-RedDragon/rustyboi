645b7c1b26132fa6687f1b34badf6109204d13f6
Jacob McSwain <jacob@mcswain.dev>
2025-09-16 19:58:48 -0500

    more opcodes

--- src/cpu/opcodes/opcodes.rs
+++ src/cpu/opcodes/opcodes.rs
@@ -12,6 +12,20 @@ pub fn jp_imm(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
     16
 }
 
+pub fn ret(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    cpu.registers.pc = mmio.read(cpu.registers.sp) as u16;
+    cpu.registers.pc |= (mmio.read(cpu.registers.sp + 1) as u16) << 8;
+    cpu.registers.sp += 2;
+    16
+}
+
+pub fn cpl(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
+    cpu.registers.a = !cpu.registers.a;
+    cpu.registers.set_flag(registers::Flag::Negative, true);
+    cpu.registers.set_flag(registers::Flag::HalfCarry, true);
+    4
+}
+
 pub fn di(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
     cpu.registers.ime = false;
     4
@@ -20,6 +34,61 @@ pub fn di(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
 pub fn ei(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
     cpu.registers.ime = true;
     4
+}
+
+pub fn ld_memory_hl_imm(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let addr = ((cpu.registers.h as u16) << 8) | (cpu.registers.l as u16);
+    let value = mmio.read(cpu.registers.pc);
+    mmio.write(addr, value);
+    cpu.registers.pc += 1;
+    12
+}
+
+pub fn ld_memory_imm_a_16(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let low = mmio.read(cpu.registers.pc) as u16;
+    let high = mmio.read(cpu.registers.pc + 1) as u16;
+    let addr = (high << 8) | low;
+    mmio.write(addr, cpu.registers.a);
+    cpu.registers.pc += 2;
+    16
+}
+
+pub fn ld_sp_imm(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let low = mmio.read(cpu.registers.pc) as u16;
+    let high = mmio.read(cpu.registers.pc + 1) as u16;
+    let value = (high << 8) | low;
+    cpu.registers.sp = value;
+    cpu.registers.pc += 2;
+    12
+}
+
+pub fn ld_a_memory_hl_inc(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let addr = ((cpu.registers.h as u16) << 8) | (cpu.registers.l as u16);
+    cpu.registers.a = mmio.read(addr);
+    let new_addr = addr.wrapping_add(1);
+    cpu.registers.h = (new_addr >> 8) as u8;
+    cpu.registers.l = (new_addr & 0x00FF) as u8;
+    8
+}
+
+pub fn ld_memory_c_a(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let addr = 0xFF00 | (cpu.registers.c as u16);
+    mmio.write(addr, cpu.registers.a);
+    8
+}
+
+pub fn call_imm(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+    let low = mmio.read(cpu.registers.pc) as u16;
+    let high = mmio.read(cpu.registers.pc + 1) as u16;
+    let addr = (high << 8) | low;
+    cpu.registers.pc += 2;
+
+    cpu.registers.sp = cpu.registers.sp.wrapping_sub(2);
+    mmio.write(cpu.registers.sp, (cpu.registers.pc & 0x00FF) as u8);
+    mmio.write(cpu.registers.sp + 1, (cpu.registers.pc >> 8) as u8);
+
+    cpu.registers.pc = addr;
+    24
 }
 
 pub fn cp_imm(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
@@ -122,20 +191,6 @@ macro_rules! make_inc_memory {
     };
 }
 
-macro_rules! make_and_register {
-    ($name:ident, $reg:ident) => {
-        pub fn $name(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
-            let result = cpu.registers.a & cpu.registers.$reg;
-            cpu.registers.a = result;
-            cpu.registers.set_flag(registers::Flag::Zero, result == 0);
-            cpu.registers.set_flag(registers::Flag::Negative, false);
-            cpu.registers.set_flag(registers::Flag::HalfCarry, true);
-            cpu.registers.set_flag(registers::Flag::Carry, false);
-            4
-        }
-    };
-}
-
 macro_rules! make_pop_register_pair {
     ($name:ident, $reg1:ident, $reg2:ident) => {
         pub fn $name(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
@@ -149,19 +204,36 @@ macro_rules! make_pop_register_pair {
     };
 }
 
-macro_rules! make_xor_register {
-    ($name:ident, $reg:ident) => {
+macro_rules! make_alu_register {
+    ($name:ident, $op:tt, $reg:ident, $is_sub:expr) => {
         pub fn $name(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
-            let result = cpu.registers.a ^ cpu.registers.$reg;
+            let result = cpu.registers.a $op cpu.registers.$reg;
             cpu.registers.a = result;
             cpu.registers.set_flag(registers::Flag::Zero, result == 0);
-            cpu.registers.set_flag(registers::Flag::Negative, false);
+            cpu.registers.set_flag(registers::Flag::Negative, $is_sub);
             cpu.registers.set_flag(registers::Flag::HalfCarry, false);
             cpu.registers.set_flag(registers::Flag::Carry, false);
             4
         }
     };
 }
+
+macro_rules! make_alu_mem_hl {
+    ($name:ident, $op:tt, $is_sub:expr) => {
+        pub fn $name(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+            let addr = ((cpu.registers.h as u16) << 8) | (cpu.registers.l as u16);
+            let value = mmio.read(addr);
+            let result = cpu.registers.a $op value;
+            cpu.registers.a = result;
+            cpu.registers.set_flag(registers::Flag::Zero, result == 0);
+            cpu.registers.set_flag(registers::Flag::Negative, $is_sub);
+            cpu.registers.set_flag(registers::Flag::HalfCarry, false);
+            cpu.registers.set_flag(registers::Flag::Carry, false);
+            8
+        }
+    };
+}
+
 
 macro_rules! make_ld_16_bit_imm {
     ($name:ident, $reg1:ident, $reg2:ident) => {
@@ -192,6 +264,88 @@ macro_rules! make_jr_cond {
     };
 }
 
+macro_rules! make_dec_combined_register {
+    ($name:ident, $reg1:ident, $reg2:ident) => {
+        pub fn $name(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
+            let value = ((cpu.registers.$reg1 as u16) << 8) | (cpu.registers.$reg2 as u16);
+            let new_value = value.wrapping_sub(1);
+            cpu.registers.$reg1 = (new_value >> 8) as u8;
+            cpu.registers.$reg2 = (new_value & 0x00FF) as u8;
+            8
+        }
+    };
+}
+
+macro_rules! make_ld_register_register {
+    ($name:ident, $reg1:ident, $reg2:ident) => {
+        pub fn $name(cpu: &mut cpu::SM83, _mmio: &mut memory::mmio::MMIO) -> u8 {
+            cpu.registers.$reg1 = cpu.registers.$reg2;
+            4
+        }
+    };
+}
+
+macro_rules! make_ld_memory_combined_register_a {
+    ($name:ident, $reg1:ident, $reg2:ident) => {
+        pub fn $name(cpu: &mut cpu::SM83, mmio: &mut memory::mmio::MMIO) -> u8 {
+            let addr = ((cpu.registers.$reg1 as u16) << 8) | (cpu.registers.$reg2 as u16);
+            mmio.write(addr, cpu.registers.a);
+            8
+        }
+    };
+}
+
+make_ld_memory_combined_register_a!(ld_memory_bc_a, b, c);
+make_ld_memory_combined_register_a!(ld_memory_de_a, d, e);
+make_ld_register_register!(ld_a_b, a, b);
+make_ld_register_register!(ld_a_c, a, c);
+make_ld_register_register!(ld_a_d, a, d);
+make_ld_register_register!(ld_a_e, a, e);
+make_ld_register_register!(ld_a_h, a, h);
+make_ld_register_register!(ld_a_l, a, l);
+make_ld_register_register!(ld_a_a, a, a);
+make_ld_register_register!(ld_b_a, b, a);
+make_ld_register_register!(ld_b_b, b, b);
+make_ld_register_register!(ld_b_c, b, c);
+make_ld_register_register!(ld_b_d, b, d);
+make_ld_register_register!(ld_b_e, b, e);
+make_ld_register_register!(ld_b_h, b, h);
+make_ld_register_register!(ld_b_l, b, l);
+make_ld_register_register!(ld_c_a, c, a);
+make_ld_register_register!(ld_c_b, c, b);
+make_ld_register_register!(ld_c_c, c, c);
+make_ld_register_register!(ld_c_d, c, d);
+make_ld_register_register!(ld_c_e, c, e);
+make_ld_register_register!(ld_c_h, c, h);
+make_ld_register_register!(ld_c_l, c, l);
+make_ld_register_register!(ld_d_a, d, a);
+make_ld_register_register!(ld_d_b, d, b);
+make_ld_register_register!(ld_d_c, d, c);
+make_ld_register_register!(ld_d_d, d, d);
+make_ld_register_register!(ld_d_e, d, e);
+make_ld_register_register!(ld_d_h, d, h);
+make_ld_register_register!(ld_d_l, d, l);
+make_ld_register_register!(ld_e_a, e, a);
+make_ld_register_register!(ld_e_b, e, b);
+make_ld_register_register!(ld_e_c, e, c);
+make_ld_register_register!(ld_e_d, e, d);
+make_ld_register_register!(ld_e_e, e, e);
+make_ld_register_register!(ld_e_h, e, h);
+make_ld_register_register!(ld_e_l, e, l);
+make_ld_register_register!(ld_h_a, h, a);
+make_ld_register_register!(ld_h_b, h, b);
+make_ld_register_register!(ld_h_c, h, c);
+make_ld_register_register!(ld_h_d, h, d);
+make_ld_register_register!(ld_h_e, h, e);
+make_ld_register_register!(ld_h_h, h, h);
+make_ld_register_register!(ld_h_l, h, l);
+make_ld_register_register!(ld_l_a, l, a);
+make_ld_register_register!(ld_l_b, l, b);
+make_ld_register_register!(ld_l_c, l, c);
+make_ld_register_register!(ld_l_d, l, d);
+make_ld_register_register!(ld_l_e, l, e);
+make_ld_register_register!(ld_l_h, l, h);
+make_ld_register_register!(ld_l_l, l, l);
 make_inc_register!(inc_a, a);
 make_inc_register!(inc_b, b);
 make_inc_register!(inc_c, c);
@@ -208,6 +362,9 @@ make_dec_register!(dec_e, e);
 make_dec_register!(dec_h, h);
 make_dec_register!(dec_l, l);
 make_dec_register!(dec_sp, sp);
+make_dec_combined_register!(dec_bc, b, c);
+make_dec_combined_register!(dec_de, d, e);
+make_dec_combined_register!(dec_hl, h, l);
 make_ld_register_imm!(ld_a_imm, a);
 make_ld_register_imm!(ld_b_imm, b);
 make_ld_register_imm!(ld_c_imm, c);
@@ -216,21 +373,47 @@ make_ld_register_imm!(ld_e_imm, e);
 make_ld_register_imm!(ld_h_imm, h);
 make_ld_register_imm!(ld_l_imm, l);
 make_inc_memory!(inc_memory_hl, h, l);
-make_and_register!(and_a, a);
-make_and_register!(and_b, b);
-make_and_register!(and_c, c);
-make_and_register!(and_d, d);
-make_and_register!(and_e, e);
-make_and_register!(and_h, h);
-make_and_register!(and_l, l);
+make_alu_register!(add_a, +, a, false);
+make_alu_register!(add_b, +, b, false);
+make_alu_register!(add_c, +, c, false);
+make_alu_register!(add_d, +, d, false);
+make_alu_register!(add_e, +, e, false);
+make_alu_register!(add_h, +, h, false);
+make_alu_register!(add_l, +, l, false);
+make_alu_register!(and_a, &, a, false);
+make_alu_register!(and_b, &, b, false);
+make_alu_register!(and_c, &, c, false);
+make_alu_register!(and_d, &, d, false);
+make_alu_register!(and_e, &, e, false);
+make_alu_register!(and_h, &, h, false);
+make_alu_register!(and_l, &, l, false);
+make_alu_register!(or_a, |, a, false);
+make_alu_register!(or_b, |, b, false);
+make_alu_register!(or_c, |, c, false);
+make_alu_register!(or_d, |, d, false);
+make_alu_register!(or_e, |, e, false);
+make_alu_register!(or_h, |, h, false);
+make_alu_register!(or_l, |, l, false);
+make_alu_register!(xor_a, ^, a, false);
+make_alu_register!(xor_b, ^, b, false);
+make_alu_register!(xor_c, ^, c, false);
+make_alu_register!(xor_d, ^, d, false);
+make_alu_register!(xor_e, ^, e, false);
+make_alu_register!(xor_h, ^, h, false);
+make_alu_register!(xor_l, ^, l, false);
+make_alu_register!(sub_a, -, a, true);
+make_alu_register!(sub_b, -, b, true);
+make_alu_register!(sub_c, -, c, true);
+make_alu_register!(sub_d, -, d, true);
+make_alu_register!(sub_e, -, e, true);
+make_alu_register!(sub_h, -, h, true);
+make_alu_register!(sub_l, -, l, true);
+make_alu_mem_hl!(add_memory_hl, +, false);
+make_alu_mem_hl!(and_memory_hl, &, false);
+make_alu_mem_hl!(or_memory_hl, |, false);
+make_alu_mem_hl!(xor_memory_hl, ^, false);
+make_alu_mem_hl!(sub_memory_hl, -, true);
 make_pop_register_pair!(pop_af, a, f);
-make_xor_register!(xor_a, a);
-make_xor_register!(xor_b, b);
-make_xor_register!(xor_c, c);
-make_xor_register!(xor_d, d);
-make_xor_register!(xor_e, e);
-make_xor_register!(xor_h, h);
-make_xor_register!(xor_l, l);
 make_ld_16_bit_imm!(ld_bc_imm, b, c);
 make_ld_16_bit_imm!(ld_de_imm, d, e);
 make_ld_16_bit_imm!(ld_hl_imm, h, l);
