0beebb0b88e928af1de76f0363128384eb0daab8
Jacob McSwain <jacob@mcswain.dev>
2026-07-16 15:47:25 -0500

    fix(android): pin :romscan module to JVM 17

--- android/romscan/build.gradle.kts
+++ android/romscan/build.gradle.kts
@@ -7,9 +7,8 @@ plugins {
     jacoco
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
